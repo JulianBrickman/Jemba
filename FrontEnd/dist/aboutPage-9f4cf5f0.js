3baa8708622c467cdb7bd19178aa66cef5d0ae42
Benjamin Falkner <bfalkner9@gmail.com>
2023-11-16 11:34:55 -0500

    new dist

--- FrontEnd/dist/aboutPage-9f4cf5f0.js
+++
@@ -1,6 +0,0 @@
-import{x as e,s as r}from"./navbar-1e50daf5.js";import"./router-c070dd98.js";class t extends r{render(){return e`
-  
-
-
-
-  `}static get properties(){return{}}constructor(){super()}}customElements.define("about-page",t);export{t as AboutPage};
